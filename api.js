040071ef9692f8151d3b25c29e043dfd5a403ac7
Yuna Hu <yunaa.765@gmail.com>
2024-05-25 10:46:18 -0700

    Remove comments

--- api.js
+++ api.js
@@ -2,8 +2,6 @@ const myButton = document.querySelector("#myButton");
 const display = document.querySelector("#display");
 
 myButton.addEventListener("click", async () => {
-  console.log("clicked!");
-
   const fetched = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
   const ditto = await fetched.json();
   const content = JSON.stringify(ditto);
